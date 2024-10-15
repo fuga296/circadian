d65df0ec2ef383185129d9a229972ffecaa8da26
fuga296 <158001951+fuga296@users.noreply.github.com>
2024-10-15 23:48:28 +0900

    Update Warning.js

--- frontend/src/components/cheering/Warning.js
+++ frontend/src/components/cheering/Warning.js
@@ -1,7 +1,7 @@
 const Warning = () => {
     return (
         <div>
-            <h1>撮影と連絡以外で</span><span>のスマホの使用は禁止ですよ</span></h1>
+            <h1><span>撮影と連絡以外で</span><span>のスマホの使用は禁止ですよ</span></h1>
         </div>
     )
 }

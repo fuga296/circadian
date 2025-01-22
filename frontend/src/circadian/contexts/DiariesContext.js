735e35a0612ddb37d9e0989fc993eea2637f53c3
fuga296 <fu121gam1729@gmail.com>
2025-01-22 21:41:23 +0900

    remove old contexts and create new contexts

--- frontend/src/circadian/contexts/DiariesContext.js
+++ frontend/src/circadian/contexts/DiariesContext.js
@@ -1,5 +1,36 @@
-import { createContext } from "react";
+import React, { createContext, useCallback, useEffect, useState } from 'react';
+import { getDiaryBlocks } from '../services/api';
+import { removeDuplicate } from '../utils/diary';
 
-const DiariesContext = createContext([]);
+export const diariesContext = createContext();
 
-export default DiariesContext;
+export const DiariesProvider = ({ children }) => {
+    const [diaries, setDiaries] = useState([]);
+
+    const fetchDiaries = useCallback(async () => {
+        try {
+            const response = await getDiaryBlocks(1);
+            if (response?.data) {
+                setDiaries(prev => removeDuplicate([ ...prev, ...response.data ], "date"));
+            } else {
+                // a
+            }
+        } catch (err) {
+            console.error("Error fetching diary:", err);
+            // handleUpdateDiaryState('error', "Failed to fetch diary. Please try again later.");
+        } finally {
+            // handleUpdateDiaryState('loading', false);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        fetchDiaries();
+    }, [fetchDiaries])
+
+    return (
+        <diariesContext.Provider value={{ diaries, setDiaries }}>
+            {children}
+        </diariesContext.Provider>
+    );
+};

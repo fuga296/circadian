b46a56a8a447ec055eb1c8cb172684f15154fd1d
fuga296 <fu121gam1729@gmail.com>
2025-01-22 21:33:50 +0900

    rename DiariesContext.js

--- frontend/src/circadian/contexts/UserInfoContext.js
+++ frontend/src/circadian/contexts/UserInfoContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useCallback, useEffect, useState } from 'react';
+import { getUserInfo } from '../services/api';
 
 export const userInfoContext = createContext();
 
@@ -7,6 +8,28 @@ export const UserInfoProvider = ({ children }) => {
         username: "",
         email: "",
     });
+
+    const fetchUserInfo = useCallback(async () => {
+        try {
+            const response = await getUserInfo();
+            if (response?.data) {
+                setUserInfo({ ...response.data });
+            } else {
+                // setUserInfo({});
+            }
+        } catch (err) {
+            console.error("Error fetching diary:", err);
+            // handleUpdateDiaryState('error', "Failed to fetch diary. Please try again later.");
+        } finally {
+            // handleUpdateDiaryState('loading', false);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchUserInfo();
+    }, [fetchUserInfo]);
+
+
     return (
         <userInfoContext.Provider value={{ userInfo, setUserInfo }}>
             {children}
